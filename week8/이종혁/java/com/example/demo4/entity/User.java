86d0ba0e2671eba6721d34f4d4576ce7305974ff
사용자 이름 <dlwhdgur0509@gmail.com>
2025-11-17 22:39:21 +0900

    [8주차] 학습 과제 제출

---
+++ week8/이종혁/java/com/example/demo4/entity/User.java
@@ -0,0 +1,17 @@
+package com.example.demo4.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+}
