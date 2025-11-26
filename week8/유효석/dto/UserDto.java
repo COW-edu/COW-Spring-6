7eb2735b758055b389ea1247812fe3c43c494448
hughryu1125 <hughr1125@gmail.com>
2025-11-26 11:49:05 +0900

    fix: UserDto

--- week8/유효석/dto/UserDto.java
+++ week8/유효석/dto/UserDto.java
@@ -5,6 +5,7 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import com.example.entity.User;
+import com.exaple.entity.UserRole
 
 @Getter
 @Builder
@@ -16,17 +17,18 @@ public class UserDto { // data를 front-end로 전달
     private Long id;
     private String name;
     private String email;
+    private UserRole role;
 
 
     public User toEntity(){
 
-        User user = User.builder().id(id).name(name).email(email).build();
+        User user = User.builder().id(id).name(name).email(email).userRole(role).build();
         return user;
     }
 
     public static UserDto from(User user){
         return UserDto.builder().id(user.getId()).name(user.getName())
-                .email(user.getEmail()).build();
+                .email(user.getEmail()).role(user.getUserRole()).build();
     }
 
 
